41df4a5be1cee39566b3962d378df94e768097f8
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-07-24 23:26:58 -0400

    Book model class implemented

---
+++ src/main/java/com/booklet/books/model/Book.java
@@ -0,0 +1,83 @@
+package com.booklet.books.model;
+
+public class Book {
+
+	private int book_id; 
+	private String author;
+	private String title;
+	private int year;
+	private boolean sellable;
+	private String printing;
+
+	// EMPTY CONSTRUCTOR
+	public Book() {
+		super();
+	}
+
+	// CONSTRUCTOR WITH FIELDS
+	public Book(int book_id, String author, String title, int year, boolean sellable, String printing) {
+		super();
+		this.book_id = book_id;
+		this.author = author;
+		this.title = title;
+		this.year = year;
+		this.sellable = sellable;
+		this.printing = printing;
+	}
+
+	// GETTERS AND SETTERS
+	public int getBook_id() {
+		return book_id;
+	}
+
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isSellable() {
+		return sellable;
+	}
+
+	public void setSellable(boolean sellable) {
+		this.sellable = sellable;
+	}
+
+	public String getPrinting() {
+		return printing;
+	}
+
+	public void setPrinting(String printing) {
+		this.printing = printing;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [book_id=" + book_id + ", author=" + author + ", title=" + title + ", year=" + year + ", sellable="
+				+ sellable + ", printing=" + printing + "]";
+	}
+	
+}
---
+++ src/main/java/com/booklet/books/model/BookMapper.java
@@ -0,0 +1,25 @@
+package com.booklet.books.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class BookMapper implements RowMapper<Book> {
+
+	@Override
+	public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Book book = new Book();
+		
+		book.setBook_id(rs.getInt("book_id"));
+		book.setAuthor(rs.getString("author"));
+		book.setTitle(rs.getString("title"));
+		book.setYear(rs.getInt("year"));
+		book.setSellable(rs.getBoolean("selleable"));
+		book.setPrinting(rs.getString("printing"));
+
+		return book;
+	}
+
+}
