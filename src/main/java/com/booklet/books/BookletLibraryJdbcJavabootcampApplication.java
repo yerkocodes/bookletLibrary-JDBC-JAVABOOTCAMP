88c9ea0094750787ad0647c1c24f9815e0daf2ac
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-07-24 22:16:07 -0400

    First Commit

---
+++ src/main/java/com/booklet/books/BookletLibraryJdbcJavabootcampApplication.java
@@ -0,0 +1,13 @@
+package com.booklet.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookletLibraryJdbcJavabootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookletLibraryJdbcJavabootcampApplication.class, args);
+	}
+
+}
